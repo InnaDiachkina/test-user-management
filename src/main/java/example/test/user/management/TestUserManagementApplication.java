13e8e08222c97c13250d9a49f7a73080d98d8863
Inna Diachkina <innadyachkina@gmail.com>
2024-05-10 12:40:21 +0300

    added pom.xml and other files

---
+++ src/main/java/example/test/user/management/TestUserManagementApplication.java
@@ -0,0 +1,13 @@
+package example.test.user.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestUserManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestUserManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/example/test/user/management/TestUserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package example.test.user.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestUserManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
